acc26714508dfb2525ce9b86bd27879c8bbccce4
june <brainyjun@gmail.com>
2021-04-25 16:33:01 +0900

    카카오 2020 신규 아이디 추천 문제

---
+++ src/kakao/Kakao2020-new-id.kt
@@ -0,0 +1,45 @@
+package kakao
+
+// // 카카오 2020 신규 아이디 추천
+// https://programmers.co.kr/learn/courses/30/lessons/72410
+
+fun main() {
+    var id = "...!@bat#*..y.abcdefghijklm"
+    println(Solution1().solution(id))
+}
+
+class Solution1 {
+    fun solution(new_id: String): String {
+        var id = new_id
+
+        // 1단계
+        id = id.toLowerCase()
+
+        // 2단계
+        id = id.replace("[^a-z\\d\\-._]".toRegex(), "")
+
+        // 3단계
+        id = id.replace("[.]{2,}".toRegex(), ".")
+
+        // 4단계
+        id = id.replace("^[.]|[.]$".toRegex(), "")
+
+        // 5단계
+        if (id.isBlank()) id = "a"
+
+        // 6단계
+        if (id.length >= 16) id = id.substring(0, 15)
+        while (id.isNotBlank() && id.last() == '.') {
+            id = id.dropLast(1)
+        }
+
+        // 7단계
+        if (id.length <= 2) {
+            while (id.length < 3) {
+                id += id.last()
+            }
+        }
+
+        return id
+    }
+}
